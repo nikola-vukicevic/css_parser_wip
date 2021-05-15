73dbe095f2aee6fccbc89d14c33b6651470e9cc1
Nikola Vukićević <39658013+nikola-vukicevic@users.noreply.github.com>
2021-05-15 07:41:48 +0200

    Add files via upload

---
+++ mapa_vrednosti.js
@@ -0,0 +1,13 @@
+var MAPA_VREDNOSTI = new Map([
+
+	["box-shadow",      ["temp",   []]],
+	["color",           ["temp",   []]],
+	["display",         ["spisak", ["block", "inline-block", "block", "flex"]]],
+	["font-family",     ["temp",   []]],
+	["margin",          ["temp",   []]],
+	["outline",         ["temp",   []]],
+	["padding",         ["temp",   []]],
+	["src",             ["temp",   []]],
+	["text-decoration", ["temp",   ["none", "underline"]]],
+
+]);
